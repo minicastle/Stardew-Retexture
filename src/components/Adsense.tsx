ddb01e0f689b48181f5f502f206fa3bec9b1a0c5
minicastle <jk598200167@gmail.com>
2025-08-12 17:34:07 +0900

    adsense test

---
+++ src/components/Adsense.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+const MainContainer = styled.div`
+  display: block;
+`;
+function Adsense() {
+  return <MainContainer></MainContainer>;
+}
+
+export default Adsense;
